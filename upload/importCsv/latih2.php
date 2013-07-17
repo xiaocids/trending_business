f388b80d079e087a802de5e08434d5977719be25
XiaoCIds <ocidsactive@gmail.com>
2013-07-17 08:57:13 +0700

    Upload CHANGELOG, index-test.php, index.php dan folder upload

---
+++ upload/importCsv/latih2.php
@@ -0,0 +1,14 @@
+<?php
+                $paramsArray = array(
+                    "table"=>"opini",
+                    "delimiter"=>",",
+		    "textDelimiter"=>"\"",
+                    "mode"=>1,
+                    "perRequest"=>10,
+                    "csvKey"=>"1",
+                    "tableKey"=>"idstr",
+                    "columns"=>array(
+                        "opini_id"=>1, "tweet_id"=>"", "idstr"=>"", "screen_name"=>"", "text"=>"", "label"=>2, "skor"=>"", "kategori_id"=>"", "kota_id"=>"", "location"=>"", "longitude"=>"", "latitude"=>"", "created_at"=>"", "create_time"=>"", 
+                    ),
+                );
+            ?>
---
+++ upload/importCsv/tb_kata_dasar(1).php
@@ -0,0 +1,14 @@
+<?php
+                $paramsArray = array(
+                    "table"=>"kata_dasar",
+                    "delimiter"=>",",
+		    "textDelimiter"=>"\"",
+                    "mode"=>1,
+                    "perRequest"=>10,
+                    "csvKey"=>"1",
+                    "tableKey"=>"katadasar_id",
+                    "columns"=>array(
+                        "katadasar_id"=>1, "kata_dasar_teks"=>2, "kata_dasar_tipe"=>3, 
+                    ),
+                );
+            ?>
